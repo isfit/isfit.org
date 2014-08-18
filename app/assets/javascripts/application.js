b54c7681c3a108e092b8d107d9f4faade0e02169
chrpeter <christian150790@gmail.com>
2014-08-18 19:28:16 +0200

    adds design changes, pages, tabs, awesome-fonts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,23 +14,79 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap
+//= require bootstrap/scrollspy
 //= require_tree .
-$(document).ready(function () {
+/**$(document).ready(function () {
 
     var menu = $('.navbar');
-    var origOffsetY = menu.offset().top+30;
+    var origOffsetY = menu.offset().top+35;
 
     function scroll() {
         if ($(window).scrollTop() >= origOffsetY) {
             $('.navbar').addClass('navbar-fixed-top');
+
             //$('.content').addClass('menu-padding');
         } else {
             $('.navbar').removeClass('navbar-fixed-top');
+            //$('.content').removeClass('menu-padding');
         }
 
 
     }
 
     document.onscroll = scroll;
+
+});*/
+/**$(document).ready(function () {  
+  var top = $('.navbar').offset().top - parseFloat($('.navbar').css('marginTop').replace(/auto/, 100));
+  $(window).scroll(function (event) {
+    // what the y position of the scroll is
+    var y = $(this).scrollTop();
+
+    // whether that's below the form
+    if (y >= top) {
+      // if so, ad the fixed class
+      $('.navbar').addClass('navbar-fixed-top');
+    } else {
+      // otherwise remove it
+      $('.navbar').removeClass('navbar-fixed-top');
+    }
+  });
+});*/
+
+ready = function () {
+  console.log("hei");
+  $('ul.nav-left li a').bind('click', function() {
+    console.log("HALLA");
+    var link = $(this).attr('href');
+    var posi = $(link).offset().top - 54;
+    $('body,html').animate({scrollTop:posi},700);
+    return false;
+  });
+  //you know the position.
+  $(window).scroll(function () { 
+    if ($(window).scrollTop() > 99) {
+      $('.navbar').addClass('navbar-fixed-top');
+      $("body").css("padding-top", 54);
+    }
+    if ($(window).scrollTop() < 100) {
+      $("body").css("padding-top", 0);
+      $('.navbar').removeClass('navbar-fixed-top');
+    }
+  });
+
+
+  $('body').scrollspy({
+    offset: 200
+  });
+}
+$(document).ready(ready)
+$(document).on('page:load', ready)
+
+$(document).ready(function() {
+  //change the integers below to match the height of your upper dive, which I called
+  //banner.  Just add a 1 to the last number.  console.log($(window).scrollTop())
+  //to figure out what the scroll position is when exactly you want to fix the nav
+  //bar or div or whatever.  I stuck in the console.log for you.  Just remove when
 
 });
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,22 @@ module ApplicationHelper
 	def tabs
 		Tab.all.order(:order)
 	end
-	def nav_link(link_text, link_path)
-	  class_name = current_page?(link_path) ? 'active' : 'tabs'
+	def nav_link(link_text, link_path, tab_id)
+		if(link_path.eql?('tabs'))
+			new_link_path = link_path
+			class_name = current_page?((tab_path(tab_id))) ? 'current' : 'tabs'
+			content_tag(:li, :class => class_name) do
+	    	link_to link_text, tab_path(tab_id)
+	  	end
+		else
+			new_link_path = link_path
+			class_name = current_page?(new_link_path.to_sym) ? 'current' : 'tabs'
+	  	content_tag(:li, :class => class_name) do
+	    	link_to link_text, new_link_path.to_sym
+	  	end
+		end
 
-	  content_tag(:li, :class => class_name) do
-	    link_to link_text, link_path
-	  end
+
 	end
 	def language_logo
 		I18n.locale.to_s.eql?("no") ? "english-logo" : "norwegian-logo" 
@@ -15,5 +25,29 @@ module ApplicationHelper
 	def language_link
 		I18n.locale.to_s.eql?("no") ? "?locale=en" : "?locale=no"
 	end
+	def format(str)
+    set_correct_tags(str)
+    yt_tag(str)
+    reg = Regexp.new(/\[([\w ,:"]+)\]\(([A-Za-z0-9_:.=&+-?\/]+)\)/)
+    str.gsub!(reg, "<a href=\"\\2\" target=\"_blank\">\\1</a>")
+
+    bc = BlueCloth.new(str) 
+    #str.gsub!(/([^>])\s*\r\n\r\n\s*([^<])/m, "\\1<br /><br />\\2")
+    #str.gsub!(/([^>])\s*\r\n\s*([^<])/m, "\\1<br />\\2")
+    #str
+
+    text = bc.to_html
+    #set_correct_tags(text)
+    text
+  end
+
+  def set_correct_tags(text)
+    # text.gsub!(/##pic (\d+) (\d+) pic##/) {|match|  article_image($1, $2, false)}
+    # text.gsub!(/#l#pic (\d+) (\d+) pic#l#/) {|match| article_image($1, $2, true)}
+  end
+
+  def yt_tag(text)
+    text.gsub!(/##yt (\w+) yt##/, "<iframe id='ytplayer' type='text/html' width='580' height='302' src='http://www.youtube.com/embed/\\1?showinfo=0&controls=0' frameborder='0' allowfullscreen></iframe>")
+  end
 
 end
