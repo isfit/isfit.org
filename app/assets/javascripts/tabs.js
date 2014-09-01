f4dfad9c3e39d13643b005a90c10ab4b0888e151
chrpeter <christian150790@gmail.com>
2014-09-01 22:18:11 +0200

    fixes css on frontpage

--- app/assets/javascripts/tabs.js
+++ app/assets/javascripts/tabs.js
@@ -1,26 +1,4 @@
 
-
- /**var scrollspy = function(){
-    	var scrollPos = $(document).scrollTop();
-    	$('#sidebar-navbar a').each(function () {
-        	var currLink = $(this);
-        	var refElement = $(currLink.attr("href"));
-        	if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-            	$('#sidebar-navbar ul li').removeClass("active"); //added to remove active class from all a elements
-            	currLink.addClass("active");
-        	}
-        	else{
-            	currLink.removeClass("active");
-        	}
-    	});
-}
-
-$(document).bind('scroll', scrollspy);
-
- scrollspy();*/
- /*$(function() { 
- 	$.scrollIt(); 
- });*/
 ready = function () {
   $(function() {
     var $cn_list    = $('#workshop-list');
