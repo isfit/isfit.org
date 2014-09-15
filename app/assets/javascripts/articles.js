f22865ecaa60dd7b35bb515c917843b1397e4b85
Thomas Gjerde <thomasgjerde90@gmail.com>
2014-09-15 21:59:15 +0200

    Adds working twitter count and facebook meta tags

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -2,7 +2,7 @@
 
 var ready;
 ready = function() {
-    $.getJSON("http://graph.facebook.com/?id=" + "http://isfit.org"/**window.location.href*/, function (json){
+    $.getJSON("http://graph.facebook.com/?id=" + "http://isfit.org" + window.location.href, function (json){
         if(typeof json.shares === 'undefined'){
           $('#fb-counter').text(0);
         }
@@ -11,15 +11,27 @@ ready = function() {
           $('#fb-counter').text(json.shares);
         }
     });
-    $.getJSON("http://urls.api.twitter.com/1/urls/count.json?url=" + "http://www.isfit.org"/**window.location.href*/, function (json){
-        if(typeof json.count === 'undefined'){
-          $('#twitter-counter').text(0);
-        }
-        else{
-          console.log("hei " + json.count );
-          $('#twitter-counter').text(json.count);
-        }
-    });
+    function getTwitterCount(url) {
+        $.ajax({
+            method: "GET",
+            url: url,
+            dataType: "jsonp",
+            jsonp: "callback",
+            jsonpCallback: "jsonpcallback",
+            success: function (json) {
+                console.log("Test");
+                if(typeof json.count === 'undefined'){
+                    $('#twitter-counter').text(0);
+                }
+                else{
+                    console.log("hei " + json.count );
+                    $('#twitter-counter').text(json.count);
+                }
+            }
+        });
+    }
+    getTwitterCount("http://urls.api.twitter.com/1/urls/count.json?url=" + "http://www.isfit.org" + window.location.href);
+
     
 };
 $(document).ready(ready);
