f392d392a16dfc0af4e882fd29cefea45b40bd0d
Thomas Gjerde <thomasgjerde90@gmail.com>
2014-09-15 22:54:49 +0200

    Adds correct count to second counter as well

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -4,11 +4,13 @@ var ready;
 ready = function() {
     $.getJSON("http://graph.facebook.com/?id=" + window.location.href, function (json){
         if(typeof json.shares === 'undefined'){
-          $('#fb-counter').text(0);
+          $('#fb-counter1').text(0);
+          $('#fb-counter2').text(0);
         }
         else{
           console.log("hei " + json.shares );
-          $('#fb-counter').text(json.shares);
+          $('#fb-counter1').text(json.shares);
+          $('#fb-counter2').text(json.shares);
         }
     });
     function getTwitterCount(url) {
@@ -21,11 +23,13 @@ ready = function() {
             success: function (json) {
                 console.log("Test");
                 if(typeof json.count === 'undefined'){
-                    $('#twitter-counter').text(0);
+                    $('#twitter-counter1').text(0);
+                    $('#twitter-counter2').text(0);
                 }
                 else{
                     console.log("hei " + json.count );
-                    $('#twitter-counter').text(json.count);
+                    $('#twitter-counter1').text(json.count);
+                    $('#twitter-counter2').text(json.count);
                 }
             }
         });
